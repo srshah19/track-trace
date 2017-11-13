41a84a7e0f2b319ef9e36563f95a7181527962cb
Snehal Shah <snehal@yourwanda.com>
2017-11-13 01:41:12 -0800

    Added a nomatch route and add switch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route, Switch
 } from 'react-router-dom';
 
 // Component Imports
 import Home from './components/Home';
 import Results from './components/Results';
+import NoMatch from "./components/NoMatch";
 import './App.css';
 
 const BasicExample = () => (
   <Router>
     <div className="brand-banner">
-      <Route exact path="/" component={Home}/>
-      <Route path="/bookings/:landingId" component={Results}/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/bookings/:landingId" component={Results}/>
+        <Route component={NoMatch}/>
+      </Switch>
     </div>
   </Router>
 );
---
+++ src/components/NoMatch.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class NoMatch extends Component{
+  render(){
+    return (
+      <h1>No Match found. Check the Url or <a href="/">Go back home!</a></h1>
+    )
+  }
+}
+
+export default NoMatch;
