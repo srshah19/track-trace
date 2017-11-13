b5c4cb395c02acda7ce17e51acbec9d5a6ffa2aa
Snehal Shah <snehal@yourwanda.com>
2017-11-12 18:15:07 -0800

    Adding more features to the app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
+import {Grid, FormGroup, FormControl, ControlLabel, HelpBlock, Row, Col} from 'react-bootstrap';
 
 class Home extends Component {
   constructor(props) {
@@ -11,26 +12,41 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        <label>Track a container</label>
-        <br />
-        <input type="text" placeholder="Enter tracking number here"
-               onChange={(event) => this.setState({searchParam: event.target.value})}/>
-        <br />
-        <Button param={this.state.searchParam}/>
-      </div>
+      <Grid>
+        <Row>
+          <Col sm={6} className="testing">
+            <FormGroup
+              bsSize="lg"
+              controlId="formBasicText">
+              <ControlLabel className="color-white font-large">Track Container</ControlLabel>
+              <FormControl
+                type="text"
+                bsSize="sm"
+                value={this.state.value}
+                placeholder="Enter booking reference number"
+                onChange={(event) => this.setState({searchParam: event.target.value})}
+              />
+              <FormControl.Feedback/>
+              <HelpBlock>You will be redirected to the results page.</HelpBlock>
+              <Button param={this.state.searchParam}/>
+            </FormGroup>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
 
 const Button = ({param}) => (
-  <Route render={({ history}) => (
+  <Route render={({history}) => (
     <button
-      type='button'
-      onClick={() => { history.push('/results/' + param) }}>
+      type='button' className="btn-primary"
+      onClick={() => {
+        history.push('/results/' + param)
+      }}>
       Search
     </button>
-  )} />
+  )}/>
 );
 
 export default Home;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
+import {Grid, Col, Row, Thumbnail, Button} from 'react-bootstrap';
 import data from '../data/data';
+import containerImage from '../container.png';
+import {CopyToClipboard} from "react-copy-to-clipboard";
+import {ToastContainer, toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
 
 class Results extends Component {
   constructor(props) {
@@ -7,13 +12,15 @@ class Results extends Component {
     this.state = {
       landingId: props.match.params.landingId,
       noResults: false,
-      data: null
+      data: null,
+      copied: false,
+      value: window.location.href || props.location.pathname
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const checkResults = data.filter(number => number['booking_number'] === this.props.match.params.landingId);
-    if(checkResults.length === 0){
+    if (checkResults.length === 0) {
       this.setState({
         noResults: true
       })
@@ -24,37 +31,70 @@ class Results extends Component {
     }
   }
 
+  notify = () => toast("URL has been copied to clipboard");
+
   render() {
     return (
       <div>
-        <h2 className="color-white">Showing results for: {this.state.landingId} </h2>
-        {this.state.noResults ? <h3>No results found</h3>:
-          (!this.state.data ? <p>Loading</p>:<Container data={this.state.data} /> )}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          pauseOnHover
+        />
         <a href="/">Home</a>
+        <h2 className="color-white">Showing results for: {this.state.landingId}
+          <CopyToClipboard text={this.state.value}
+                           onCopy={() => this.setState({copied: true})}>
+            <span className="m-l-2 glyphicon glyphicon-share-alt" onClick={this.notify}/>
+          </CopyToClipboard>
+        </h2>
+        {this.state.noResults ? <h3>No results found</h3> :
+          (!this.state.data ? <p>Loading</p> : <Container data={this.state.data}/> )}
       </div>
     )
   }
 }
 
 class Container extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
-  render(){
+
+  render() {
     return (
-      this.props.data.map((item,i) =>
-        <div>
-          <p className="color-white font-large">Steamship Line: {item.steamship_line}</p>
-          <p className="color-white font-large">Origin: {item.origin}</p>
-          <p className="color-white font-large">Destination: {item.destination}</p>
-          <br />
-          <h4>Containers: </h4>
-        {item.containers.map((container, i ) =>
-          <div className="shipping-container">
-            <span key={i}>{container.number}</span>
-          </div>
-        )}
-        </div>)
+      <Grid>
+        {this.props.data.map((item, i) =>
+          <Row className="show-grid" key={i}>
+            <p className="color-white font-large">Steamship Line: {item.steamship_line}</p>
+            <p className="color-white font-large">Origin: {item.origin}</p>
+            <p className="color-white font-large">Destination: {item.destination}</p>
+            <br/>
+            <h4>Containers: </h4>
+            <Grid>
+              <Row>
+                {item.containers.map((container, i) =>
+                  <Col xs={6} md={4} key={i}>
+                    <Thumbnail src={containerImage} alt="242x200" className="container-img">
+                      <h5>Container Number: <strong>{container.number}</strong></h5>
+                      <p>Size: {container.size}</p>
+                      <p>Type: {container.type}</p>
+                      <p>Current Location: {container.location}</p>
+                      <p>Last Status at: {container.last_status_at}</p>
+                      <p>Last Status: {container.last_status}</p>
+                      <p>
+                        <Button bsStyle="primary">Show Updates</Button>&nbsp;
+                        <Button bsStyle="default">Hide Updates</Button>
+                      </p>
+                    </Thumbnail>
+                  </Col>
+                )}
+              </Row>
+            </Grid>
+          </Row>)}
+      </Grid>
     )
   }
 }
